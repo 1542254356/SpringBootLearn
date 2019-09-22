e173c156097946d9db1a9d65416d8db781d077d5
hjwblog.com <yysz169hjw@126.com>
2019-09-22 23:08:56 +0800

    feat: SpringBoot 启动后自动打开默认浏览器

---
+++ springstart/src/main/java/com/hjwblog/springstart/config/AutoStartProjectInDefaultBrowser.java
@@ -0,0 +1,26 @@
+package com.hjwblog.springstart.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * SpringBoot 项目启动之后自动启动默认浏览器
+ */
+
+@SpringBootConfiguration
+public class AutoStartProjectInDefaultBrowser implements CommandLineRunner {
+    @Value("${server.port}")
+    private String port;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        String path = "/";
+        try {
+            Runtime.getRuntime().exec("cmd /c start http://localhost:" + port + path);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
